d2907c8d6fc50e02ea214cc9af2ecf140029c84c
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-11-16 20:24:20 -0600

    Added user model

--- domain/model/user.go
+++ domain/model/user.go
@@ -1,1 +1,16 @@
 package model
+
+import "time"
+
+//User defines the structure of a DB user
+type User struct {
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Name      string    `json:"name"`
+	Age       string    `json:"age"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+//TableName returns the name of the table
+func (User) TableName() string { return "users" }
